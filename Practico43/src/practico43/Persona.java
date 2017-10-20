95bd7b5480e59c6d6c42bb9df247e292553715ee
Morthem <morthem@live.com>
2017-10-20 19:53:58 -0300

    clase Persona

--- Practico43/src/practico43/Persona.java
+++ Practico43/src/practico43/Persona.java
@@ -1,9 +1,41 @@
 
 package practico43;
+import practico43.Telefonia.*;
 
 public class Persona {
     
     private long dni;
     private String nombre;
+    
+    public Persona(long dni, String nombre)
+    {
+        this.dni=dni;
+        this.nombre=nombre;
+    }
+    
+    public void llamarMensajear(Telefono telefono, long nroDestino, String mensaje)
+    {
+        
+    }
+    
+    public void llamarMensajear(Telefono telefono, long nroDestino)
+    {
+        
+    }
+    
+    
+    
+    public long getDni() {
+        return dni;
+    }
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
